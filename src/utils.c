a702140ce6ae6bf5c95fb5e642fdb5f7fdd47eaf
Iaio99 <glnvlln0@gmail.com>
2023-02-16 18:08:10 +0100

    Implemented functions init_string.h and concatenate_string

--- src/utils.c
+++ src/utils.c
@@ -1,10 +1,46 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "utils.h"
 
 void error(char *message, int error_code)
 {
-    perrror(stderr, "Input invalid\n");
+    perror(message);
     exit(error_code);
+}
+
+
+char *init_string(char flag, const char *parameter)
+{
+    size_t len = 3 + sizeof(flag) + strlen(parameter);
+    char *buffer = calloc(len, sizeof(char));
+    
+    buffer[0] = '-';
+    buffer[1] = flag;
+    buffer[2] = ' ';
+    strcpy(&buffer[3], parameter);
+    buffer[len-1] = ' ';
+
+    return buffer;
+}
+
+char *concat_string(size_t rc, ...) 
+{
+    char *dst = calloc(rc, sizeof(char));
+    char *src;
+    va_list ap;
+    va_start(ap, rc);
+
+    while(rc > 0) {
+        src = va_arg(ap, char *);
+        strcat(dst, src);
+        rc -= strlen(src);
+    }
+
+    va_end(ap);
+    dst[rc-1] = 0;
+
+    return dst;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,7 @@
 #pragma once
 
-void error(char *message, int error_code);
+#include <stdarg.h>
+
+extern void error(char *message, int error_code);
+extern char *init_string(char flag, const char *parameter);
+extern char *concat_string(size_t rc, ...);
