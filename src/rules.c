adb53195ee87d71e0ec00b2e288ef4cd7c8002b9
Iaio99 <glnvlln0@gmail.com>
2023-02-16 18:07:38 +0100

    Refactoring and implemented case 'j' and case 't'

--- src/rules.c
+++ src/rules.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #include "rules.h"
+#include "utils.h"
 
 
 static const char *get_table(table_t table)
@@ -54,6 +55,7 @@ static const char *get_chain(chain_t chain)
             chain_name = "POSTROUTING";
             break;
         default:
+            chain_name = "";
             break;
     }
 
@@ -78,7 +80,7 @@ static const char *get_rule(rule_t rule) {
             rule_name = "RETURN";
             break;
         default:
-
+            rule_name = "";
             break;
     }
 
@@ -88,7 +90,6 @@ static const char *get_rule(rule_t rule) {
 
 char *make_rule(const char *fmt, ...)
 {
-//    char cmd[1024] = {0};
 	int rc = 9;
     char *cmd;
 	va_list ap;
@@ -111,34 +112,28 @@ char *make_rule(const char *fmt, ...)
             case 'V':
             case 'I':
             case 'R':
-                chain = calloc(strlen(get_chain(va_arg(ap, chain_t))) + 3, sizeof(chain_t));
-                chain[0] = '-';
-                chain[1] = *fmt;
-                chain[2] = ' ';
-                strcpy(&chain[3], get_chain(va_arg(ap, chain_t)));
+                chain = init_string(*fmt, get_chain(va_arg(ap, chain_t)));
                 rc += strlen(chain);
                 break;
 
-            case "t":
-                table = calloc(strlen(get_table(va_arg(ap, table_t))) + 3, sizeof(table_t));
-                table[0] = '-';
-                table[1] = *fmt;
-                table[2] = ' ';
-                strcpy(&table[3], get_table(va_arg(ap, table_t)));
+            case 't':
+                table = init_string(*fmt, get_table(va_arg(ap, table_t)));
                 rc += strlen(table);
+                break;
 
-            case "j":
-                rule = calloc(strlen(get_rule(va_arg(ap, rule_t))) + 3, sizeof(rule_t));
-                rule[0] = '-';
-                rule[1] = *fmt;
-                rule[2] = ' ';
-                strcpy(&rule[3], get_rule(va_arg(ap, rule_t)));
+            case 'j':
+                rule = init_string(*fmt, get_rule(va_arg(ap, rule_t)));
                 rc += strlen(rule);
+                break;
 		}
 		fmt++;
 	}
+    
 	va_end(ap);
+    cmd = concat_string(rc, "iptables ", table, chain, rule);
+    free(table);
+    free(chain);
+    free(rule);
 
-    cmd = chain;
 	return cmd;
 }
